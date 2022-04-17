0e757b834a81f30bd2210f1d121f6fdbeea1cbd5
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-04-17 11:10:52 +0300

    Update

--- l_sh_work/GoLang_Training/go-ps-process/gops.go
+++ l_sh_work/GoLang_Training/go-ps-process/gops.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	ps "github.com/mitchellh/go-ps"
+	"strings"
+	"os"
 )
 
 func main(){
@@ -11,12 +13,23 @@ func main(){
 		fmt.Println("ps.Processes() Failed, are you using windows?")
 		return
 	}
-
+	//fmt.Println(len(os.Args), os.Args)
+	var refString = "thunar"
+	if (len(os.Args)>1) {
+		//fmt.Println(len(os.Args), os.Args[1])
+		refString = os.Args[1]
+	}
+	//const refString = "thunar"
 	// map ages
 	for x := range processList {
 		var process ps.Process
 		process = processList[x]
-		fmt.Printf("%d\t%s\n",process.Pid(),process.Executable())
+		contain := strings.Contains(strings.ToLower(process.Executable()),strings.ToLower(refString))
+		if (contain) {
+			fmt.Printf("%d ",process.Pid())
+		}
+		//fmt.Printf("%d\t%s\n",process.Pid(),process.Executable())
 		// do os.* stuff on the pid
 	}
+	fmt.Printf("\n")
 }
