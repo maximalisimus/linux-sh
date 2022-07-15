ac17b545423654fe88b94a1ce2b030a9663831f6
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-15 15:28:27 +0300

    Update

--- l_sh_work/Python_Training/Py-Teaching/classes.py
+++ l_sh_work/Python_Training/Py-Teaching/classes.py
@@ -5,18 +5,18 @@ class A(object):
 		self.__pole = pole
 	
 	@property
-	def pole(self):
+	def Pole(self):
 		return self.__pole
 	
-	@pole.setter
-	def pole(self, value):
+	@Pole.setter
+	def Pole(self, value):
 		if type(value) == str:
 			self.__pole = value
 		else:
 			raise TypeError('Не верный тип данных! Введите строчный тип данных!')
 
-	@pole.deleter
-	def pole(self):
+	@Pole.deleter
+	def Pole(self):
 		del self.__pole
 
 class B(A):
@@ -25,12 +25,12 @@ class B(A):
 		super(B if type(pole) == str else '', self).__init__(pole)
 	
 	def display(self):
-		print(self.pole)
+		print(self.Pole)
 
 def main():
 	my = B('stroka')
 	my.display()
-	my.pole = '123'
+	my.Pole = '123'
 	my.display()
 
 if __name__ == '__main__':
