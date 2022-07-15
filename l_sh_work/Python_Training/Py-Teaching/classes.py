03fd79b714a6d754a78999eff9208fe690f502af
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-07-15 11:37:44 +0300

    Update

---
+++ l_sh_work/Python_Training/Py-Teaching/classes.py
@@ -0,0 +1,34 @@
+
+class A(object):
+	
+	def __init__(self, pole):
+		self.__pole = pole
+	
+	@property
+	def pole(self):
+		return self.__pole
+	
+	@pole.setter
+	def pole(self, value):
+		self.__pole = value
+
+	@pole.deleter
+	def pole(self):
+		del self.__pole
+
+class B(A):
+	
+	def __init__(self, pole):
+		super(B, self).__init__(pole)
+	
+	def display(self):
+		print(self.pole)
+
+def main():
+	my = B('stroka')
+	my.display()
+	my.pole = 'my string'
+	my.display()
+
+if __name__ == '__main__':
+	main()
