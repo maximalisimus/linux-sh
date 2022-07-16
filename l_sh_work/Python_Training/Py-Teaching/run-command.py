7d3192d3f68a296e7055af3b4afe0200096bdbdc
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-16 12:32:54 +0300

    Update

---
+++ l_sh_work/Python_Training/Py-Teaching/run-command.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+def main():
+	#process = subprocess.run(['ls', '-l', '-a'])
+	#process = subprocess.run(['ls', '-l', '-a'], capture_output=True)
+	#print(process.returncode)
+	#print(process.stdout.decode('UTF-8'))
+	#process = subprocess.run('ls -al $HOME/002', shell=True)
+	
+
+if __name__ == '__main__':
+	main()
