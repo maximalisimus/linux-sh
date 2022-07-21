ce42f088273d00f8e096497da15cd2b259e84e85
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-21 15:53:10 +0300

    Update

--- l_sh_work/Python_Training/Py-Teaching/status.py
+++ l_sh_work/Python_Training/Py-Teaching/status.py
@@ -7,10 +7,11 @@ mydir = str(pathlib.Path(pathlib.Path.cwd()).resolve().joinpath("parent"))
 print(filemode(p.stat().st_mode))
 print(oct(S_IMODE(p.stat().st_mode)), '\n')
 
+pathlib.Path(mydir).mkdir(parents=True, exist_ok=True)
+
 print(filemode(pathlib.Path(mydir).stat().st_mode))
 print(oct(S_IMODE(pathlib.Path(mydir).stat().st_mode)), '\n')
 
-pathlib.Path(mydir).mkdir(parents=True, exist_ok=True)
 pathlib.Path(mydir).chmod(0o700)
 
 print(filemode(pathlib.Path(mydir).stat().st_mode))
