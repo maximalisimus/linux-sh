a71d452c52b7f2aedb322c0746f8b8bcc949a1d8
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-21 15:12:35 +0300

    Update

---
+++ l_sh_work/Python_Training/Py-Teaching/status.py
@@ -0,0 +1,17 @@
+﻿import pathlib
+from stat import S_IMODE, filemode
+
+p = pathlib.Path( 'color.py' )
+mydir = str(pathlib.Path(pathlib.Path.cwd()).resolve().joinpath("parent"))
+
+print(filemode(p.stat().st_mode))
+print(oct(S_IMODE(p.stat().st_mode)), '\n')
+
+print(filemode(pathlib.Path(mydir).stat().st_mode))
+print(oct(S_IMODE(pathlib.Path(mydir).stat().st_mode)), '\n')
+
+pathlib.Path(mydir).mkdir(parents=True, exist_ok=True)
+pathlib.Path(mydir).chmod(0o700)
+
+print(filemode(pathlib.Path(mydir).stat().st_mode))
+print(oct(S_IMODE(pathlib.Path(mydir).stat().st_mode)))
