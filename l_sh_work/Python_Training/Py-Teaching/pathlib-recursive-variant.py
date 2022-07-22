c811abc9f82a32171a656d226dd5ddfcbcba490f
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-22 17:29:48 +0300

    Update

--- l_sh_work/Python_Training/Py-Teaching/pathlib-recursive-variant.py
+++ l_sh_work/Python_Training/Py-Teaching/pathlib-recursive-variant.py
@@ -1,12 +1,23 @@
-from pathlib import Path
+import pathlib
 
-p = Path('./')
+import shutil
+
+my_dir = './Primer'
+my_dir1 = './Primer2'
+
+p = pathlib.Path(my_dir)
 
 for child in p.iterdir():
 	print(str(child))
 
-#paths = sorted(Path('.').glob('**/*'))
-paths = Path('.').glob('**/*')
+paths = sorted(pathlib.Path(my_dir).glob('**/*'))
 
 for i in paths:
 	print(str(i))
+
+try:
+	shutil.rmtree(my_dir1)
+except FileNotFoundError as e:
+	#print(e.message, e.args)
+	pass
+
