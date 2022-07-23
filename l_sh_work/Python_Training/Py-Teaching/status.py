023ebd37289c0fb93e9fdb8e3c58addf620b9fb4
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-23 09:59:15 +0300

    Update

--- l_sh_work/Python_Training/Py-Teaching/status.py
+++ l_sh_work/Python_Training/Py-Teaching/status.py
@@ -1,18 +1,16 @@
 ﻿import pathlib
 from stat import S_IMODE, filemode
 
-p = pathlib.Path( 'color.py' )
-mydir = str(pathlib.Path(pathlib.Path.cwd()).resolve().joinpath("parent"))
+p = pathlib.Path( 'strukture.txt' )
+print(p.stat().st_mode)
+print(filemode(p.stat().st_mode))
+print(oct(S_IMODE(p.stat().st_mode)))
+
+mod = '0o755' # oct: str
+a = int(mod, 8)
+print(mod, a, oct(a))
+
+pathlib.Path(p).chmod(a)
 
 print(filemode(p.stat().st_mode))
-print(oct(S_IMODE(p.stat().st_mode)), '\n')
-
-pathlib.Path(mydir).mkdir(parents=True, exist_ok=True)
-
-print(filemode(pathlib.Path(mydir).stat().st_mode))
-print(oct(S_IMODE(pathlib.Path(mydir).stat().st_mode)), '\n')
-
-pathlib.Path(mydir).chmod(0o700)
-
-print(filemode(pathlib.Path(mydir).stat().st_mode))
-print(oct(S_IMODE(pathlib.Path(mydir).stat().st_mode)))
+print(oct(S_IMODE(p.stat().st_mode)))
